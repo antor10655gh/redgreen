a71541100e3d819784786a893591e775bf2da160
antor10655gh <creativefreelancerantor@gmail.com>
2022-09-14 17:19:30 +0600

    add others menu

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,14 +1,15 @@
 import { Menu } from 'antd';
-import { AppstoreFilled, FlagFilled, DatabaseFilled } from '@ant-design/icons';
+import { AppstoreFilled, FlagFilled, DatabaseFilled, SettingFilled, UserOutlined, ExclamationCircleFilled } from '@ant-design/icons';
 import React from 'react';
 
 const Sidebar = () => {
     return (
         <div>
-            <h3 style={{marginTop: "20px", marginLeft: "40px"}} class="menu-title">MENU</h3>
+            
             <Menu 
                 defaultSelectedKeys={['dashboard']}
             >
+                <h3 style={{margin: "15px", marginTop:"30px"}} class="menu-title">MENU</h3>
                 <Menu.Item key="dashboard">
                     <AppstoreFilled />
                     <span>Dashboard</span>
@@ -20,6 +21,19 @@ const Sidebar = () => {
                 <Menu.Item key="menu">
                     <DatabaseFilled />
                     <span>Menu</span>
+                </Menu.Item>
+                <h3 style={{margin: "15px", marginTop:"30px"}} class="menu-title">OTHERS</h3>
+                <Menu.Item key="settings">
+                    <SettingFilled />
+                    <span>Settings</span>
+                </Menu.Item>
+                <Menu.Item key="accounts">
+                    <UserOutlined />
+                    <span>Accounts</span>
+                </Menu.Item>
+                <Menu.Item key="helps">
+                    <ExclamationCircleFilled />
+                    <span>Helps</span>
                 </Menu.Item>
             </Menu>
         </div>
